0f756d2be24f71a1e6d8495a29a30d950a523413
mancuoj <mancuoj@gmail.com>
2025-05-02 23:07:51 +0800

    chore: nr check

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,15 @@
 import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'React Router' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ]
+  return [{ title: 'React Router' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <button className="btn btn-primary">React Router</button>
+      <button type="button" className="btn btn-primary">
+        React Router
+      </button>
     </div>
   )
 }
