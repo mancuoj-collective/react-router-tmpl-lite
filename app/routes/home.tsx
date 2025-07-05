37d697cb9c51a4da986c294b6e32c243b2a72d38
mancuoj <mancuoj@gmail.com>
2025-07-05 13:35:28 +0800

    chore: update deps

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,15 +1,22 @@
 import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: 'React Router' }, { name: 'description', content: 'Welcome to React Router!' }]
+  return [
+    { title: 'React Router' },
+    { name: 'description', content: 'React router starter template' },
+  ]
 }
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <button type="button" className="btn btn-primary">
-        <a href="https://github.com/mancuoj-collective/remix-tmpl-lite" target="_blank" rel="noopener noreferrer">
-          React Router
+        <a
+          href="https://github.com/mancuoj-collective/remix-tmpl-lite"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
         </a>
       </button>
     </div>
